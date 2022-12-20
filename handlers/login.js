6fa530fc4892d50d3550572a648a7f678cd12f4f
Pampapathi <pampa.vandli@gmail.com>
2022-12-20 23:24:45 +0530

    Added jwt secret key

--- handlers/login.js
+++ handlers/login.js
@@ -2,8 +2,9 @@ const express = require('express');
 const Router = express.Router();
 const token = require('../utils/token');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URL;
+const uri = process.env.MONGODBDEV_URL;
 const passwordUtils = require('../utils/utils');
+
 const DB_NAME = 'password-shell';
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -23,8 +24,14 @@ Router.post('/', async (req, res) => {
       (await passwordUtils.comparePassword(password, resp.password))
     ) {
       // Generate jwt token
-      const jwt = token.generateToken({ name: 'Pampapathi', age: 29 });
-      res.send({ jwt });
+      const jwt = token.generateToken({ email, username: resp.username });
+      res.cookie('token', jwt, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        expires: new Date(Date.now() + 14400000),
+      });
+      res.send('SUCESS');
     } else {
       res.status(401).send('Invalid credentials');
     }
@@ -40,6 +47,7 @@ Router.post('/signup', async (req, res) => {
   try {
     const hashedPassword = await passwordUtils.hashPassword(password);
     const userData = {
+      username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
       OTP: null,
@@ -59,9 +67,24 @@ Router.post('/signup', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
-
-Router.get('/auth', (req, res) => {
-  const jwt = token.generateToken({ name: 'Pampapathi', age: 29 });
-  res.send({ jwt });
+Router.get('/whoami', async (req, res) => {
+  const { cookies } = req;
+  // TODO: Not working when trying to fetch cookies jwt
+  // Please work on it first
+  console.log(req.cookies);
+  if (Object.keys(cookies).length === 0) {
+    return res.status(403).send('Forbidden');
+  }
+  const reqJwt = cookies.token;
+  try {
+    const details = await token.verifyToken(reqJwt);
+    if (details) {
+      res.status(200).send(details);
+    } else {
+      res.status(403).send('FORBIDDEN');
+    }
+  } catch (error) {
+    res.status(403).send('FORBIDDEN');
+  }
 });
 module.exports = Router;
--- utils/token.js
+++ utils/token.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
-const PRIVATE_KEY = 'husdhfbwhivbiwvbsd';
+const PRIVATE_KEY = process.env.JWT_SECRETE_KEY;
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, PRIVATE_KEY, { expiresIn: '1h' });
+  return jwt.sign(payload, PRIVATE_KEY, { expiresIn: '1d' });
 };
 const verifyToken = (token) => {
   return jwt.verify(token, PRIVATE_KEY);
